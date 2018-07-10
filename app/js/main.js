cf6bca186b300062e6a8231dd2de44a8348d2d05
gpasha <g1ebikpasha@gmail.com>
2018-07-11 01:37:09 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+
+
+    //form animation
+
+    var form = document.querySelector(".form");
+    var footer = document.querySelector(".footer");
+
+    footer.onwheel = function () {
+        form.classList.add("form-active");
+    };
+
+
+    //send info
+
+    var btn = document.querySelector(".form__btn");
+    var btnSent = document.querySelector(".form__sending");
+
+    btn.onsubmit = function () {
+        btnSent.style.display = "inline";
+    };
+
+};
